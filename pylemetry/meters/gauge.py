413fa25cd416281123b5f3191bec7d80b049a179
Alex Murphy <alex@weaverslodge.co.uk>
2025-09-13 14:49:00 +0100

    Add thread-safe locking to gauge and counter meters

--- pylemetry/meters/gauge.py
+++ pylemetry/meters/gauge.py
@@ -1,18 +1,25 @@
+from threading import Lock
+
+
 class Gauge:
     def __init__(self):
+        self.lock = Lock()
         self.value = 0.0
 
     def get_value(self) -> float:
         return self.value
 
     def set_value(self, value: float):
-        self.value = value
+        with self.lock:
+            self.value = value
 
     def add(self, value: float) -> None:
-        self.value += value
+        with self.lock:
+            self.value += value
 
     def subtract(self, value: float) -> None:
-        self.value -= value
+        with self.lock:
+            self.value -= value
 
     def __add__(self, other: float) -> "Gauge":
         self.add(other)
